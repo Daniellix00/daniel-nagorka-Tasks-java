d2a76acb3cffeea8d726c90616cbfcc3c31b4a24
Daniel Nagorka <danielosnag1996@gmail.com>
2024-01-20 12:37:52 +0100

    Controller

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,6 +5,9 @@ import com.crud.tasks.domain.TaskDTo;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -23,29 +26,36 @@ public class TaskController {
         this.taskMapper = taskMapper;
     }
     @GetMapping(value =  "{taskId}")
-    public TaskDTo getTaskById(@PathVariable Long id){
-        Optional<Task> taskOptional = service.getTaskById(id);
-        return taskOptional.map(taskMapper::mapToTaskDto).orElse(null);
-    }
-    @GetMapping
-    public List<TaskDTo> getTasks() {
-        List<Task> tasks = service.getAllTasks();
-        return taskMapper.mapToTaskDtoList(tasks);
-    }
-@GetMapping(value =  "{taskId}")
-    public TaskDTo getTask( @PathVariable Long taskId) {
-        return new TaskDTo(1L, "test title", "test_content");
-    }
-@DeleteMapping
-    public void deleteTask(Long taskId) {
+       public ResponseEntity<TaskDTo> getTask(@PathVariable Long taskId) throws  TaskNotFoundException {
 
+        return ResponseEntity.ok(taskMapper.mapToTaskDto(service.getTask(taskId)));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskDTo>> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return ResponseEntity.ok(taskMapper.mapToTaskDtoList(tasks));
+   }
+//@GetMapping(value =  "{taskId}")
+  // public TaskDTo getTask( @PathVariable Long taskId) {
+     //   return new TaskDTo(1L, "test title", "test_content");
+    //}
+@DeleteMapping(value = "{taskId}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) throws TaskNotFoundException {
+        service.delete(taskId);
+return   ResponseEntity.ok().build();
     }
 @PutMapping
-    public TaskDTo updateTask(TaskDTo taskDto) {
-        return new TaskDTo(1L, "Edited test title", "Test content");
+    public ResponseEntity<TaskDTo> updateTask(@RequestBody TaskDTo taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        Task savedTask = service.saveTask(task);
+        return ResponseEntity.ok(taskMapper.mapToTaskDto(savedTask));
     }
-@PostMapping
-    public void createTask(TaskDTo taskDto) {
+@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createTask(@RequestBody TaskDTo taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        service.saveTask(task);
+        return ResponseEntity.ok().build();
 
     }
 }
