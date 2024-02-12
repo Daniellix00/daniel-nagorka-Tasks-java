f8195b5601f61dc20eca7aa0d6e2cfa30124681f
Daniel Nagorka <danielosnag1996@gmail.com>
2024-02-12 19:00:04 +0100

    Test

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,12 +1,12 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.client.TrelloClient;
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,16 +19,11 @@ public class TrelloController {
     private final TrelloClient trelloClient;
 
     @GetMapping("boards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-     List<TrelloBoardDto>filteredBoard = trelloBoards.stream()
-             .filter(board->board.getId()!=null && board.getName()!=null)
-             .filter(board->board.getName().contains("Kodilla"))
-             .collect(Collectors.toList());
-for (TrelloBoardDto board: filteredBoard){
-    System.out.println(board);
-}
+   public ResponseEntity<List<TrelloBoardDto>> getTrelloBoards(){
+        return ResponseEntity.ok(trelloClient.getTrelloBoards());
+    }
+    @PostMapping("cards")
+    public ResponseEntity<CreatedTrelloCard>createTrelloCard(@RequestBody TrelloCardDto trelloCardDto){
+        return ResponseEntity.ok(trelloClient.createNewCard(trelloCardDto));
     }
 }
--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -4,22 +4,24 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
+import java.net.URI;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
 @NoArgsConstructor
-public class TrelloBoardDto {
+public class CreatedTrelloCard {
     @JsonProperty("id")
     private String id;
 
     @JsonProperty("name")
     private String name;
 
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty("shortUrl")
+    private String shortUrl;
+
+
 }
